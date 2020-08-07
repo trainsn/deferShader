3bebb5618aa6c64f2edbc4d94bf3ecb547cf7c07
Neng Shi <shineng@zju.edu.cn>
2020-08-07 17:43:18 -0400

    finish shadow map

--- textureMapping/textureMapping/main.cpp
+++ textureMapping/textureMapping/main.cpp
@@ -30,15 +30,17 @@ glm::vec3 base_color(10.0 / 255.0, 10.0 / 255.0, 10.0 / 255.0);
 // settings
 const unsigned int SCR_WIDTH = 256;
 const unsigned int SCR_HEIGHT = 256;
+const unsigned int SHADOW_WIDTH = 256;
+const unsigned int SHADOW_HEIGHT = 256;
 
 glm::mat4 pMatrix;
-glm::mat4 invPMatrix;
+glm::mat4 inv_pMatrix;
 glm::mat4 mvMatrix;
+glm::mat4 inv_vMatrix;
 glm::mat3 normalMatrix;
 
 // Information related to the camera 
 float dist = 2.3;
-float theta, phi;
 glm::vec3 direction;
 glm::vec3 up;
 glm::vec3 center;
@@ -48,13 +50,17 @@ glm::mat4 view;
 glm::mat4 model;
 
 // Lighting power.
-float lighting_power = 2;
+float lighting_power = 0.0;
+float lighting_power1 = 0.4;
 
 // Used for time based animation.
 float time_last = 0;
 
 // Use lighting?
 int use_lighting = 1;
+
+// Use shadow?
+int use_shadow = 1;
 
 // Render different buffers.
 int show_depth = 0;
@@ -67,8 +73,8 @@ float point_light_phi = M_PI / 2;
 float point_light_theta_step = 0.39 / 2;
 float point_light_phi_step = 0.39;
 
-float point_light_theta1 = 1.57;
-float point_light_phi1 = 1.57;
+float point_light_theta1 = 43.0245139107 / 180.0 * M_PI;
+float point_light_phi1 = 101.1242862229 / 180.0 * M_PI;
 float point_light_theta_step1 = 0.3;
 float point_light_phi_step1 = 0.3;
 
@@ -77,9 +83,14 @@ void setMatrixUniforms(Shader ourShader) {
 	ourShader.setMat4("uPMatrix", pMatrix);
 	ourShader.setMat4("uMVMatrix", mvMatrix);
 
+	// inverse of view matrix
+	inv_vMatrix = glm::inverse(view);
+	ourShader.setMat4("uInvVMatrix", inv_vMatrix);
+
 	// inverse of projection matrix 
-	invPMatrix = glm::inverse(pMatrix);
-	ourShader.setMat4("uInvPMatrix", invPMatrix);
+	inv_pMatrix = glm::inverse(pMatrix);
+	ourShader.setMat4("uInvPMatrix", inv_pMatrix);
+
 
 	// Pass the vertex normal matrix to the shader so it can compute the lighting calculations.
 	normalMatrix = glm::transpose(glm::inverse(glm::mat3(mvMatrix)));
@@ -129,6 +140,10 @@ int main()
 	shaderLightingPass.setInt("gDiffuseColor", 2);
 	shaderLightingPass.setInt("gMask", 3);
 	shaderLightingPass.setInt("gDepth", 4);
+	shaderLightingPass.setInt("gShadowMask", 5);
+	shaderLightingPass.setInt("gShadowDepth", 6);
+	shaderLightingPass.setInt("gShadowMask1", 7);
+	shaderLightingPass.setInt("gShadowDepth1", 8);
 
 	// create the projection matrix 
 	float near = 1.79f;
@@ -177,16 +192,33 @@ int main()
 			far);
 	}
 
+	// load and create a texture 
+	// -------------------------	
+	unsigned int gPosition, gNormal, gMask, gDiffuseColor, gDepth, gShadowMask, gShadowDepth, gShadowMask1, gShadowDepth1;
+
+	char filename[1024];
+	sprintf(filename, "MPAS_000000_3.27890_20.000000_90.0000026563_100.0000018721.h5", 0);
+	string filename_s = filename;
+	int last_dot = filename_s.rfind(".");
+	int last_dash = filename_s.rfind("_");
+	float phi = stof(filename_s.substr(last_dash + 1, last_dot - last_dash - 1));
+	phi = phi * M_PI / 180;
+	int second_last_dash = filename_s.rfind("_", last_dash - 1);
+	float theta = stof(filename_s.substr(second_last_dash + 1, last_dash - second_last_dash - 1));
+	theta = theta * M_PI / 180;
+	int third_last_dash = filename_s.rfind("_", second_last_dash - 1);
+	float isoValue = stof(filename_s.substr(third_last_dash + 1, second_last_dash - third_last_dash - 1));
+	int fourth_last_dash = filename_s.rfind("_", third_last_dash - 1);
+	float BwsA = stof(filename_s.substr(fourth_last_dash + 1, third_last_dash - fourth_last_dash - 1));
+
 	// Move to the 3D space origin.
 	mvMatrix = glm::mat4(1.0f);
 
 	// transform
-	theta = M_PI;
-	phi = 0;
-	//direction = glm::vec3(sin(theta) * cos(phi) * dist, sin(theta) * sin(phi) * dist, cos(theta) * dist);
-	//up = glm::vec3(sin(theta - M_PI / 2) * cos(phi), sin(theta - M_PI / 2) * sin(phi), cos(theta - M_PI / 2));
-	direction = glm::vec3(0.0f, dist, 0.0f);
-	up = glm::vec3(0.0f, 0.0f, 1.0f);
+	direction = glm::vec3(sin(theta) * cos(phi) * dist, sin(theta) * sin(phi) * dist, cos(theta) * dist);
+	up = glm::vec3(sin(theta - M_PI / 2) * cos(phi), sin(theta - M_PI / 2) * sin(phi), cos(theta - M_PI / 2));
+	//direction = glm::vec3(0.0f, dist, 0.0f);
+	//up = glm::vec3(0.0f, 0.0f, 1.0f);
 	center = glm::vec3(0.0f, 0.0f, 0.0f);
 	eye = center + direction;
 
@@ -196,12 +228,6 @@ int main()
 
 	mvMatrix = view * model;
 
-	// load and create a texture 
-	// -------------------------	
-	unsigned int gPosition, gNormal, gMask, gDiffuseColor, gDepth;
-
-	char filename[1024];
-	sprintf(filename, "res.h5", 0);
 	hid_t file, space3, space1, dset_position, dset_normal, dset_mask, dset_depth;
 	herr_t status;
 	hsize_t dims3[1] = { SCR_WIDTH * SCR_HEIGHT * 3 };
@@ -222,7 +248,7 @@ int main()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	float* pBuffer = new float[SCR_WIDTH * SCR_HEIGHT * 3];
 	status = H5Dread(dset_position, H5T_NATIVE_FLOAT, H5S_ALL, H5S_ALL, H5P_DEFAULT, pBuffer);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB16F, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_FLOAT, pBuffer);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB32F, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_FLOAT, pBuffer);
 
 	glGenTextures(1, &gNormal);
 	glBindTexture(GL_TEXTURE_2D, gNormal); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
@@ -231,8 +257,8 @@ int main()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	float* nBuffer = new float[SCR_WIDTH * SCR_HEIGHT * 3];
 	status = H5Dread(dset_normal, H5T_NATIVE_FLOAT, H5S_ALL, H5S_ALL, H5P_DEFAULT, nBuffer);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB16F, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_FLOAT, nBuffer);
-
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB32F, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_FLOAT, nBuffer);
+	
 	glGenTextures(1, &gMask);
 	glBindTexture(GL_TEXTURE_2D, gMask); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
 	// set texture filtering parameters
@@ -258,20 +284,23 @@ int main()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	float* cBuffer = new float[SCR_WIDTH * SCR_HEIGHT * 4];
 	for (int i = 0; i < SCR_WIDTH * SCR_HEIGHT; i++) {
-		if (mBuffer[i] == 1.0) {
-			cBuffer[i * 4 + 0] = 1.0;
-			cBuffer[i * 4 + 1] = 1.0;
-			cBuffer[i * 4 + 2] = 1.0;
-		}
-		else {
-			cBuffer[i * 4 + 0] = 0.0;
-			cBuffer[i * 4 + 1] = 0.0;
-			cBuffer[i * 4 + 2] = 0.0;
-		}
+		cBuffer[i * 4 + 0] = 1.0;
+		cBuffer[i * 4 + 1] = 1.0;
+		cBuffer[i * 4 + 2] = 1.0;
 		cBuffer[i * 4 + 3] = 1.0;
 	}		
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGBA, GL_FLOAT, cBuffer);
 
+	char shadow_filename[1024];
+	sprintf(shadow_filename, "MPAS_000000_3.27890_20.000000_90.0000026563_90.0000026563.h5");
+	filename_s = shadow_filename;
+	last_dot = filename_s.rfind(".");
+	last_dash = filename_s.rfind("_");
+	point_light_phi1 = stof(filename_s.substr(last_dash + 1, last_dot - last_dash - 1));
+	point_light_phi1 = point_light_phi1 * M_PI / 180;
+	second_last_dash = filename_s.rfind("_", last_dash - 1);
+	point_light_theta1 = stof(filename_s.substr(second_last_dash + 1, last_dash - second_last_dash - 1));
+	point_light_theta1 = point_light_theta1 * M_PI / 180;
 	// render loop
 	// -----------
 	while (!glfwWindowShouldClose(window))
@@ -282,22 +311,10 @@ int main()
 
 		// render
 		// ------
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		shaderLightingPass.use();
-
-		// Bind texture
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, gPosition);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, gNormal);
-		glActiveTexture(GL_TEXTURE2);
-		glBindTexture(GL_TEXTURE_2D, gDiffuseColor);
-		glActiveTexture(GL_TEXTURE3);
-		glBindTexture(GL_TEXTURE_2D, gMask);
-		glActiveTexture(GL_TEXTURE4);
-		glBindTexture(GL_TEXTURE_2D, gDepth);
 
 		// set lighting sources 
 		float time_now = glfwGetTime();
@@ -308,14 +325,14 @@ int main()
 				//point_light_theta += point_light_theta_step * time_delta;
 				//point_light_phi += point_light_phi_step * time_delta;
 
-				if (point_light_theta > (M_PI * 2)) point_light_theta = 0.0;
-				if (point_light_phi > (M_PI * 2)) point_light_phi = 0.0;
+				//if (point_light_theta > (M_PI * 2)) point_light_theta = 0.0;
+				//if (point_light_phi > (M_PI * 2)) point_light_phi = 0.0;
 
-				point_light_theta1 += point_light_theta_step1 * time_delta;
-				point_light_phi1 += point_light_phi_step1 * time_delta;
+				//point_light_theta1 += point_light_theta_step1 * time_delta;
+				//point_light_phi1 += point_light_phi_step1 * time_delta;
 
-				if (point_light_theta1 > (M_PI * 2)) point_light_theta1 = 0.0;
-				if (point_light_phi1 > (M_PI * 2)) point_light_phi1 = 0.0;
+				//if (point_light_theta1 > (M_PI * 2)) point_light_theta1 = 0.0;
+				//if (point_light_phi1 > (M_PI * 2)) point_light_phi1 = 0.0;
 			}
 		}
 
@@ -342,22 +359,135 @@ int main()
 			shaderLightingPass.setVec3("uAmbientColor", base_color);
 
 			// Point light 1.
-			float point_light_position_x = 0 + 13.5 * cos(point_light_theta) * sin(point_light_phi);
-			float point_light_position_y = 0 + 13.5 * sin(point_light_theta) * sin(point_light_phi);
-			float point_light_position_z = 0 + 13.5 * cos(point_light_phi);
-			glm::vec4 light_pos(point_light_position_x, point_light_position_y, point_light_position_z, 1.0);
+			float point_light_dist = 2.3;
+			glm::vec3 point_light_direction = direction / dist * point_light_dist;
+			glm::vec3 point_light_position = center + point_light_direction;
+			glm::vec4 light_pos(point_light_position.x, point_light_position.y, point_light_position.z, 1.0);
 			light_pos = view * light_pos;
 
 			shaderLightingPass.setVec3("uPointLightingColor", lighting_power, lighting_power, lighting_power);
 			shaderLightingPass.setVec3("uPointLightingLocation", light_pos[0], light_pos[1], light_pos[2]);
 
 			// Point light 2.
-			/*float point_light_position_x1 = 0 + 8.0 * cos(point_light_theta1) * sin(point_light_phi1);
-			float point_light_position_y1 = 0 + 8.0 * sin(point_light_theta1) * sin(point_light_phi1);
-			float point_light_position_z1 = 0 + 8.0 * cos(point_light_phi1);
+			float point_light_dist1 = 2.3;
+			float point_light_position_x1 = 0 + point_light_dist1 * cos(point_light_phi1) * sin(point_light_theta1);
+			float point_light_position_y1 = 0 + point_light_dist1 * sin(point_light_phi1) * sin(point_light_theta1);
+			float point_light_position_z1 = 0 + point_light_dist1 * cos(point_light_theta1);
 
-			shaderLightingPass.setVec3("uPointLightingColor1", lighting_power, lighting_power, lighting_power);
-			shaderLightingPass.setVec3("uPointLightingLocation1", point_light_position_x1, point_light_position_y1, point_light_position_z1);*/
+			glm::vec4 light_pos1(point_light_position_x1, point_light_position_y1, point_light_position_z1, 1.0);
+			light_pos1 = view * light_pos1;
+
+			shaderLightingPass.setVec3("uPointLightingColor1", lighting_power1, lighting_power1, lighting_power1);
+			shaderLightingPass.setVec3("uPointLightingLocation1", light_pos1[0], light_pos1[1], light_pos1[2]);
+
+			shaderLightingPass.setInt("uUseShadow", use_shadow);
+
+			if (use_shadow) {
+				glm::mat4 lightProjection, lightView, lightView1;
+				glm::mat4 lightSpaceMatrix, lightSpaceMatrix1;
+				lightProjection = pMatrix;
+				lightView = view;
+				lightSpaceMatrix = lightProjection * lightView;
+				
+				glm::vec3 point_light_up1 = glm::vec3(sin(point_light_theta1 - M_PI / 2) * cos(point_light_phi1), sin(point_light_theta1 - M_PI / 2) * sin(point_light_phi1), cos(point_light_theta1 - M_PI / 2));
+				lightView1 = glm::lookAt(glm::vec3(point_light_position_x1, point_light_position_y1, point_light_position_z1), center, point_light_up1);
+				lightSpaceMatrix1 = lightProjection * lightView1;
+
+				shaderLightingPass.setMat4("uMMatrix", model);
+				shaderLightingPass.setMat4("lightSpaceMatrix", lightSpaceMatrix);
+				shaderLightingPass.setMat4("lightSpaceMatrix1", lightSpaceMatrix1);
+
+				hid_t file_shadow, dset_shadow_mask, dset_shadow_depth;
+
+				// open file and dataset using the default properties, light 0
+				char filepath_shadow[1024];
+				sprintf(filepath_shadow, "%s", filename);
+
+				file_shadow = H5Fopen(filepath_shadow, H5F_ACC_RDONLY, H5P_DEFAULT);
+				dset_shadow_mask = H5Dopen(file_shadow, "mask", H5P_DEFAULT);
+				dset_shadow_depth = H5Dopen(file_shadow, "depth", H5P_DEFAULT);
+
+				// read the data using default properties, light 0
+				glGenTextures(1, &gShadowMask);
+				glBindTexture(GL_TEXTURE_2D, gShadowMask); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+				// set texture filtering parameters
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+				float* mShadowBuffer = new float[SHADOW_WIDTH * SHADOW_HEIGHT];
+				status = H5Dread(dset_shadow_mask, H5T_NATIVE_FLOAT, H5S_ALL, H5S_ALL, H5P_DEFAULT, mShadowBuffer);
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_RED, GL_FLOAT, mShadowBuffer);
+
+				glGenTextures(1, &gShadowDepth);
+				glBindTexture(GL_TEXTURE_2D, gShadowDepth); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+				// set texture filtering parameters
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+				float* dShadowBuffer = new float[SHADOW_WIDTH * SHADOW_HEIGHT];
+				status = H5Dread(dset_shadow_depth, H5T_NATIVE_FLOAT, H5S_ALL, H5S_ALL, H5P_DEFAULT, dShadowBuffer);
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_R16F, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_RED, GL_FLOAT, dShadowBuffer);
+
+				hid_t file_shadow1, dset_shadow_mask1, dset_shadow_depth1;
+
+				// open file and dataset using the default properties, light 1
+				char filepath_shadow1[1024];
+				sprintf(filepath_shadow1, "%s", shadow_filename);
+
+				file_shadow1 = H5Fopen(filepath_shadow1, H5F_ACC_RDONLY, H5P_DEFAULT);
+				dset_shadow_mask1 = H5Dopen(file_shadow1, "mask", H5P_DEFAULT);
+				dset_shadow_depth1 = H5Dopen(file_shadow1, "depth", H5P_DEFAULT);
+
+				// read the data using default properties, light 0
+				glGenTextures(1, &gShadowMask1);
+				glBindTexture(GL_TEXTURE_2D, gShadowMask1); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+				// set texture filtering parameters
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+				float* mShadowBuffer1 = new float[SHADOW_WIDTH * SHADOW_HEIGHT];
+				status = H5Dread(dset_shadow_mask1, H5T_NATIVE_FLOAT, H5S_ALL, H5S_ALL, H5P_DEFAULT, mShadowBuffer1);
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_RED, GL_FLOAT, mShadowBuffer1);
+
+				glGenTextures(1, &gShadowDepth1);
+				glBindTexture(GL_TEXTURE_2D, gShadowDepth1); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+				// set texture filtering parameters
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+				float* dShadowBuffer1 = new float[SHADOW_WIDTH * SHADOW_HEIGHT];
+				status = H5Dread(dset_shadow_depth1, H5T_NATIVE_FLOAT, H5S_ALL, H5S_ALL, H5P_DEFAULT, dShadowBuffer1);
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_R16F, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_RED, GL_FLOAT, dShadowBuffer1);
+
+				status = H5Dclose(dset_shadow_mask);
+				status = H5Dclose(dset_shadow_depth);
+				status = H5Dclose(dset_shadow_mask1);
+				status = H5Dclose(dset_shadow_depth1);
+				status = H5Fclose(file_shadow);
+				status = H5Fclose(file_shadow1);
+
+				delete mShadowBuffer;
+				delete mShadowBuffer1;
+				delete dShadowBuffer;
+				delete dShadowBuffer1;
+			}
+			// Bind texture
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, gPosition);
+			glActiveTexture(GL_TEXTURE1);
+			glBindTexture(GL_TEXTURE_2D, gNormal);
+			glActiveTexture(GL_TEXTURE2);
+			glBindTexture(GL_TEXTURE_2D, gDiffuseColor);
+			glActiveTexture(GL_TEXTURE3);
+			glBindTexture(GL_TEXTURE_2D, gMask);
+			glActiveTexture(GL_TEXTURE4);
+			glBindTexture(GL_TEXTURE_2D, gDepth);
+			if (use_shadow) {
+				glActiveTexture(GL_TEXTURE5);
+				glBindTexture(GL_TEXTURE_2D, gShadowMask);
+				glActiveTexture(GL_TEXTURE6);
+				glBindTexture(GL_TEXTURE_2D, gShadowDepth);
+				glActiveTexture(GL_TEXTURE7);
+				glBindTexture(GL_TEXTURE_2D, gShadowMask1);
+				glActiveTexture(GL_TEXTURE8);
+				glBindTexture(GL_TEXTURE_2D, gShadowDepth1);
+			}
 		}
 
 		shaderLightingPass.setInt("uUseLighting", use_lighting);
@@ -370,6 +500,24 @@ int main()
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	glDeleteTextures(1, &gPosition);
+	glDeleteTextures(1, &gNormal);
+	glDeleteTextures(1, &gDepth);
+	glDeleteTextures(1, &gMask);
+	glDeleteTextures(1, &gDiffuseColor);
+
+	status = H5Dclose(dset_position);
+	status = H5Dclose(dset_normal);
+	status = H5Dclose(dset_depth);
+	status = H5Dclose(dset_mask);
+	status = H5Fclose(file);
+
+	delete pBuffer;
+	delete nBuffer;
+	delete dBuffer;
+	delete mBuffer;
+	delete cBuffer;
 
 	// glfw: terminate, clearing all previously allocated GLFW resources.
 	// ------------------------------------------------------------------
